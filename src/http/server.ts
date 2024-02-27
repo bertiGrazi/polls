cb9f5ce8257e8d54e1eee11fe93cfb0261a81b42
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2024-02-27 14:05:24 -0300

    create post vote options router

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,11 +1,13 @@
 import fastify from 'fastify'
 import { createPoll } from './routes/create-poll'
 import { getPollParms } from './routes/get-poll'
+import { voteOnPoll } from './routes/vote-on-poll'
 
 const app = fastify()
 
 app.register(createPoll)
 app.register(getPollParms)
+app.register(voteOnPoll)
 
 app.listen({port: 3333}).then(() => {
   console.log("✅ HTTP server running!")
