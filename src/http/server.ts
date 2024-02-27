845440c9221fa18d0a81c6d24be8adc67be16dcc
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2024-02-27 10:09:03 -0300

    install docker and prisma

--- src/http/server.ts
+++ src/http/server.ts
@@ -2,10 +2,6 @@ import fastify from 'fastify'
 
 const app = fastify()
 
-app.get('/hello', () => {
-  return 'Hello NWL'
-})
-
 app.listen({port: 3333}).then(() => {
   console.log("✅ HTTP server running!")
 })
